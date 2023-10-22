17e2d1e51cf5ffbd9a928d84240cc2c1edebdbfe
yekim0325 <yeeun940325@gmail.com>
2023-10-22 09:37:12 +0900

    TimeTraceAop.jave 파일 추가
    AOP 기능 추가를 위한 파일 생성

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,14 +20,13 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 중복회원 검증
-
         /*
         Optional<Member> result = memberRepository.findByName(member.getName());
         result.ifPresent(m -> { // result가 null이 아니라 값이 있으면 예외를 발생시킴
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
          */
-        
+
         // 위에 내용에서 Optional 리턴값을 안보여주되 동일한 로직
         validateDuplicateMember(member);
         memberRepository.save(member);
@@ -45,7 +44,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
